2aac0538f29cc05f6ed5a9c36f1c6d06d996daa6
lvrach <leo.al.vra@gmail.com>
2014-12-07 17:53:46 +0200

    support offline msgs

--- app.js
+++ app.js
@@ -7,10 +7,21 @@ app.use(express.cookieParser());
 app.use(express.session({secret: 'monkey'}));
 
 var rooms = {};
+var offlineMsg = {};
 
 // Setup the ready route, join room and broadcast to room.
 app.io.route('ready', function(req) {
     req.io.join(req.session.room);
+
+    if ( req.session.name in offlineMsg[req.session.room] ) {
+        console.log('reconnecting');
+        offlineMsg[req.session.room][req.session.name]
+            .forEach(function (msg) {
+                req.io.emit('inbound', msg);
+            });
+
+        delete offlineMsg[req.session.room][req.session.name];
+    }
     req.io.room(req.session.room).broadcast('announce', {
         message: req.session.name + ' enter room.'
     });
@@ -19,11 +30,25 @@ app.io.route('ready', function(req) {
     });
 });
 
+app.io.route('disconnect', function (req) {
+    console.log(req.session.name, 'left', req.session.room);
+    if ( req.session.name && req.session.room ) {
+        offlineMsg[req.session.room][req.session.name] = [];
+    }
+});
+
 app.io.route('outbound', function(req) {
-    req.io.room(req.session.room).broadcast('inbound', {
+    
+    var msg = {
         message: req.data.message,
         from: req.session.name
-    });
+    }
+
+    req.io.room(req.session.room).broadcast('inbound', msg);
+    
+    for ( var user in offlineMsg[req.session.room] ) {
+        offlineMsg[req.session.room][user].push(msg);
+    }
 });
 
 // Send the client html.
@@ -34,17 +59,22 @@ app.get('/', function(req, res) {
 app.get('/new-room', function(req, res) {
     var roomId = shortId.generate();
     rooms[roomId] = {};
+    offlineMsg[roomId] = {};
 
     app.io.broadcast(roomId, 'announce', {
         message: req.session.name + ' join room. '
     });
 
-    res.redirect('/join/' + roomId);
+    res.redirect('/room/' + roomId);
 });
 
-app.get('/join/:roomId', function(req, res) {
+app.get('/room/:roomId', function(req, res) {
     if (!req.session.name) {
         req.session.name = eden.adam();
+    }
+    if ( ! (req.params.roomId in rooms) ) {
+        res.redirect('/new-room');
+        return;
     }
 
     req.session.room = req.params.roomId;
@@ -52,3 +82,4 @@ app.get('/join/:roomId', function(req, res) {
 });
 
 app.listen(7076)
+console.log('localhost:7076');
